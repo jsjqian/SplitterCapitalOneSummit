4662e3689c267c79b1c7b730d02bbc648391b901
jsjqian <jack.qian97@gmail.com>
2016-06-29 21:26:14 -0400

    Add everything from the project

---
+++ Splitter/src/main/java/com/parse/loginsample/basic/InGroupActivity.java
@@ -0,0 +1,14 @@
+package com.parse.loginsample.basic;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class InGroupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_group);
+    }
+
+}
